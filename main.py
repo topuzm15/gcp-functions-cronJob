cae955f5d695f08477cef20997dbfe346125f360
Muhamed Emin Topuz <muhamme.topuz@getir.com>
2022-09-29 21:06:00 +0300

    cloudbuild.yaml added

---
+++ main.py
@@ -0,0 +1,17 @@
+def http_cron_job(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/0.12/api/#flask.Flask.make_response>`.
+    """
+
+    request_json = request.get_json()
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    else:
+        return f'Hello World!'
